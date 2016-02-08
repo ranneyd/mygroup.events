443c8e78dd28e23f646e13eb981bc3edbbacde75
Dustin Kane <dustinpkane@gmail.com>
2016-02-07 21:32:14 -0800

    Actually started to make a lay

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Ravie' });
+});
+
+router.get('/group', function(req, res, next) {
+  res.render('group', { title: 'My Group' });
 });
 
 module.exports = router;
