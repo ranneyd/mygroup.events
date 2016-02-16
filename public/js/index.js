624f0dbc8674636e928336f00cab59588b761fc9
Dustin Kane <dustinpkane@gmail.com>
2016-02-16 13:10:26 -0800

    Trying to make this date picker work

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,10 @@ $(document).ready(function(){
     $(".tooltipped").click(function(){
         $(this).trigger("mouseleave");
     });
+    $('.modal-trigger').leanModal();
+    
+    $('.datepicker').pickadate({
+        selectMonths: true, // Creates a dropdown to control month
+        selectYears: 15 // Creates a dropdown of 15 years to control year
+    });
 });
