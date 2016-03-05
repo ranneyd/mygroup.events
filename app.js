08070e03e99f62e49f0654e2364fdcbe8ff3427d
Dustin Kane <dustinpkane@gmail.com>
2016-03-04 23:32:06 -0800

    Users pretty much work

--- app.js
+++ app.js
@@ -4,36 +4,47 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var mongoose = require('mongoose');
-var User = require('./models/user.js');
-var Group = require('./models/group.js');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
-mongoose.connect('mongodb://localhost:27017/ravie', function(err){
-    if (err) throw err;
-    console.log('Successfully connected to MongoDB');
-});
-
-var routes = require('./routes/index');
+var routes = require('./routes/routes');
 
 var app = express();
-var port = process.env.PORT || 8080;
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
 
+var User = require('./models/user.js');
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+mongoose.connect('mongodb://localhost:27017/ravie', function(err){
+    if (err) throw err;
+    console.log('Successfully connected to MongoDB');
+});
 
 // error handlers
 
@@ -59,5 +70,4 @@ app.use(function(err, req, res, next) {
     });
 });
 
-app.listen(port);
 module.exports = app;
--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,62 @@
 var express = require('express');
+var passport = require('passport');
 var router = express.Router();
 var Group = require('../models/group.js');
 var Banner = require('../models/banner.js');
 var Event = require('../models/event.js');
+var User = require('../models/user.js');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Ravie' });
+    res.render('index', { title: 'Ravie', user : req.user });
 });
+
+router.get('/register', function(req, res) {
+    res.render('register', { title: 'Ravie' });
+});
+
+router.post('/register', function(req, res) {
+    User.register(new User({ username : req.body.username, email: req.body.email }), req.body.password, function(err, user) {
+        if (err) {
+            return res.render('register', { user : user, error : err.message});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            res.redirect(req.session.returnTo || '/');
+        });
+    });
+});
+
+router.get('/login', function(req, res) {
+    res.render('login', { title: "Ravie", user : req.user });
+});
+
+router.post('/login', function(req, res) {
+    console.log("ayy");
+    passport.authenticate('local')(req, res, function(err) {
+        console.log("lmao");
+        console.log(err);
+        res.redirect(req.session.returnTo || '/');    
+    })
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect(req.session.returnTo || '/');
+});
+
+
+router.get(/\/ajax\/userExists\/(.+)\/?/, function(req, res) {
+    User.findOne(
+        {
+            "username" : req.params[0]
+        },
+        function(err, results){
+            res.send(!!results);
+    });
+});
+
 router.get('/ajax/getBannerImages', function(req, res, next) {
     Banner.find(
         {},
@@ -28,6 +76,7 @@ router.get('/uh-oh', function(req, res, next) {
 
 // Group page 
 router.get(/^\/([^\/]+)\/?$/, function(req, res, next) {
+    req.session.returnTo = req.path; 
     Group.find({
             "url": req.params[0]
         },
@@ -38,10 +87,10 @@ router.get(/^\/([^\/]+)\/?$/, function(req, res, next) {
                 res.redirect("/uh-oh");
             }
             if ( results.length ){
-                res.render('group', { title: results[0].name, group: true, currentUrl: req.params[0] });
+                res.render('group', { title: results[0].name, group: true, currentUrl: req.params[0], user: req.user});
             }
             else {
-                res.render('404', { title: "Ravie" });
+                res.render('404', { title: "Ravie", user : req.user });
             }
     });
 });
